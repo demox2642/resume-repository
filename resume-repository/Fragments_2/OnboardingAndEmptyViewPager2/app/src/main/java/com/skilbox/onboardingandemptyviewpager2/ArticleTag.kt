a3805fbfc9bdadee707cac4793acae5d012185e3
Anton <60138205+demox2642@users.noreply.github.com>
2021-04-15 21:14:42 +0500

    Delete resume-repository/Fragments_2 directory

--- resume-repository/Fragments_2/OnboardingAndEmptyViewPager2/app/src/main/java/com/skilbox/onboardingandemptyviewpager2/ArticleTag.kt
+++
@@ -1,8 +0,0 @@
-package com.skilbox.onboardingandemptyviewpager2
-
-enum class ArticleTag(val tagName: String) {
-    NEWS("Новости"),
-    TECHNOLOGY("Технология"),
-    POLITICS("Политика"),
-    MUSIC("Музыка")
-}
