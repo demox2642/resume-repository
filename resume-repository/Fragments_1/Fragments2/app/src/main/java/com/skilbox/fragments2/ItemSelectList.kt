33bdeb0144e215e1c623b6ff48e36dd8009834e0
Anton <60138205+demox2642@users.noreply.github.com>
2021-04-15 21:14:16 +0500

    Delete resume-repository/Fragments_1 directory

--- resume-repository/Fragments_1/Fragments2/app/src/main/java/com/skilbox/fragments2/ItemSelectList.kt
+++
@@ -1,5 +0,0 @@
-package com.skilbox.fragments2
-
-interface ItemSelectList {
-    fun onItemSelect(text: String)
-}
--- resume-repository/Fragments_1/Fragments2/app/src/main/java/com/skilbox/fragments2/LogOn.kt
+++
@@ -1,5 +0,0 @@
-package com.skilbox.fragments2
-
-interface LogOn {
-    fun newUserIsSaved(saved: Boolean)
-}
